18e48f9d042b067658e58a0441fd7b31c77cc337
gireeshgg <34162819+gireeshgg@users.noreply.github.com>
2019-07-18 17:31:25 +0530

    Update replaceip.sh

--- replaceip.sh
+++ replaceip.sh
@@ -1,3 +1,4 @@
+export TERRAFORMVM=`cat terraform/public_ip.txt`
 cat <<EOF > /etc/ansible/hosts
 [terraformhost]
 wpznv87178dns2.eastus2.cloudapp.azure.com  ansible_connection=ssh ansible_user=devopsinfra ansible_ssh_private_key_file=/home/devopsinfra/.ssh/id_rsa 
