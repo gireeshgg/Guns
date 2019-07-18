7dcec3a1f28a2aa59166f1e0dcccc87e865cded2
gireeshgg <34162819+gireeshgg@users.noreply.github.com>
2019-07-18 15:42:28 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,9 +39,11 @@ resource "aws_instance" "web" {
         instance_type = "t2.micro"
         key_name = "${var.key_name}"
         vpc_security_group_ids = ["${aws_security_group.default.id}"]
-        
         tags = {
                Name = "Gireesh"
+        }
+        provisioner "local-exec" {
+               command = "echo ${aws_instance.web.public_ip} > public_ip.txt"
         }
 }
 
