a9ab3bf6c4e50f4122e54fefcc54079c9d19c1ab
gireeshgg <34162819+gireeshgg@users.noreply.github.com>
2019-07-18 15:10:59 +0530

    Create replaceip.sh

---
+++ replaceip.sh
@@ -0,0 +1,6 @@
+cat <<EOF > /etc/ansible/hosts
+[terraformhost]
+wpznv87178dns2.eastus2.cloudapp.azure.com  ansible_connection=ssh ansible_user=devopsinfra ansible_ssh_private_key_file=/home/devopsinfra/.ssh/id_rsa 
+[terraformvm]
+${TERRAFORMVM} ansible_connection=ssh   ansible_user=ubuntu    ansible_ssh_private_key_file=/home/devopsinfra/terraform.pem
+EOF
